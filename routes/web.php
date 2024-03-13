83e12c4f850c03bb551bf57df6fb1feea81c30e7
weihung <drum831@gmail.com>
2024-03-13 16:32:14 +0800

    (feat)新增路由設定

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('Home');
+
+Route::get('/menu', function () {
+    return view('menu');
+})->name('Menu');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('About');
+
+Route::get('/book', function () {
+    return view('book');
+})->name('Book');
