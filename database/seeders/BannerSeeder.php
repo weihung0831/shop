fce851c604ef3a9a987df90f45b380c60b365474
weihung <drum831@gmail.com>
2024-03-13 21:35:01 +0800

    (feat)建立 banners table 資料

---
+++ database/seeders/BannerSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Banner;
+
+class BannerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $banners = [
+            [
+                'title' => 'Fast Food Restaurant',
+                'image' => 'imsges/hero-bg.jpg',
+                'description' => 'Doloremque, itaque aperiam facilis rerum, commodi, temporibus sapiente ad mollitia laborum quam quisquam esse error unde. Tempora ex doloremque, labore, sunt repellat dolore, iste magni quos nihil ducimus libero ipsam.',
+            ],
+        ];
+
+        foreach ($banners as $banner) {
+            Banner::create($banner);
+        }
+    }
+}
