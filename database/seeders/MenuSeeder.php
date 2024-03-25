270b37f01ee5388ff934f8924ee5b406617ca9e6
weihung <drum831@gmail.com>
2024-03-25 16:08:29 +0800

    修改為每個類別數量 2 個以上

--- database/seeders/MenuSeeder.php
+++ database/seeders/MenuSeeder.php
@@ -21,6 +21,18 @@ class MenuSeeder extends Seeder
                 'category' => 'Burger'
             ],
             [
+                'name' => 'Delicious Burger',
+                'description' => 'Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque',
+                'price' => 15,
+                'category' => 'Burger'
+            ],
+            [
+                'name' => 'Delicious Pizza',
+                'description' => 'Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque',
+                'price' => 20,
+                'category' => 'Pizza'
+            ],
+            [
                 'name' => 'Delicious Pizza',
                 'description' => 'Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque',
                 'price' => 20,
@@ -31,6 +43,18 @@ class MenuSeeder extends Seeder
                 'description' => 'Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque',
                 'price' => 18,
                 'category' => 'Pasta'
+            ],
+            [
+                'name' => 'Delicious Pasta',
+                'description' => 'Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque',
+                'price' => 18,
+                'category' => 'Pasta'
+            ],
+            [
+                'name' => 'French Fries',
+                'description' => 'Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque',
+                'price' => 10,
+                'category' => 'Fries'
             ],
             [
                 'name' => 'French Fries',
