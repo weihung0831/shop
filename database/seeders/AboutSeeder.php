85991a9e200d95bb398c4f36d8f79a2b3dd970ae
weihung <drum831@gmail.com>
2024-03-27 16:18:56 +0800

    新增 /about 頁面 migrate、model、seed 建立

---
+++ database/seeders/AboutSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\About;
+
+class AboutSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $abouts = [
+            [
+                'title' => 'We Are Feane',
+                'description' => 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don`t look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn`t anything embarrassing hidden in the middle of text. All',
+            ],
+        ];
+
+        foreach ($abouts as $about) {
+            About::create($about);
+        }
+    }
+}
